dba23370d28f374bb43c3c634ca623cf5936fcea
marleigh <volcana@gmail.com>
2021-03-19 10:38:48 -0700

    playing with gradients

---
+++ script.js
@@ -0,0 +1,20 @@
+const circle = document.querySelector(".circle");
+let deg = 45;
+
+// circle.addEventListener('mouseenter', colorChange);
+// circle.addEventListener('mouseleave', colorChange);
+
+circle.addEventListener('mousemove', colorChange);
+
+function colorChange() {
+
+    if(deg < 180){
+        deg++;
+    } else if(deg > 180){
+        deg--;
+    } else {
+        deg = 45;
+    }
+    console.log(deg);
+    circle.style.background = `linear-gradient(${deg}deg, rgba(0,0,0,1) 12%, rgba(104,121,197,1) 40%, rgba(39,226,222,1) 76%)`;
+}
