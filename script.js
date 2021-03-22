3e6c500f1416747cd5117edcefb5d72a29c44321
marleigh <volcana@gmail.com>
2021-03-22 09:21:47 -0700

    ?? its a mystery

--- script.js
+++ script.js
@@ -17,4 +17,5 @@ function colorChange() {
     }
     console.log(deg);
     circle.style.background = `linear-gradient(${deg}deg, rgba(0,0,0,1) 12%, rgba(104,121,197,1) 40%, rgba(39,226,222,1) 76%)`;
+    //circle.style.boxShadow = `box-shadow: 0 10px 20px linear-gradient(${deg}deg, rgba(104,121,197,1) 40%, rgba(39,226,222,1) 76%, rgba(0,0,0,1) 12%)`
 }
